55bed86d064afdd448969d49717302fb7f01de9c
sanjay <rsanjayraja10@gmail.com>
2026-02-18 21:22:10 +0530

    Updated project files

--- L2/STRING/length.py
+++ L2/STRING/length.py
@@ -2,11 +2,11 @@ def lengthOfLastWord(s: str) -> int:
     length = 0
     i = len(s) - 1
     
-    # Skip trailing spaces
+
     while i >= 0 and s[i] == " ":
         i -= 1
     
-    # Count last word length
+
     while i >= 0 and s[i] != " ":
         length += 1
         i -= 1
