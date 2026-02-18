a34a5e73050b2e518959f3ecb8bbb64b1c262722
sanjay <rsanjayraja10@gmail.com>
2026-02-18 21:12:48 +0530

    Initial commit

---
+++ L2/ARRAY/Twosum.py
@@ -0,0 +1,10 @@
+def twoSum(nums, target):
+    num_map = {}
+    
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        
+        if complement in num_map:
+            return [num_map[complement], i]
+        
+        num_map[nums[i]] = i
---
+++ L2/MATH/Palindrome.py
@@ -0,0 +1,13 @@
+def isPalindrome(x: int) -> bool:
+    if x < 0:
+        return False
+    
+    original = x
+    reversed_num = 0
+    
+    while x > 0:
+        digit = x % 10
+        reversed_num = reversed_num * 10 + digit
+        x = x // 10
+    
+    return original == reversed_num
